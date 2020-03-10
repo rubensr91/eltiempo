34310d749a26fa44171688ae8b4f2cb81d09b5ae
Ruben <rubensere@gmail.com>
2020-03-10 17:51:58 +0100

    first commit

---
+++ lib/eltiempo.rb
@@ -0,0 +1,6 @@
+require "eltiempo/version"
+
+module Eltiempo
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/eltiempo_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Eltiempo do
+  it "has a version number" do
+    expect(Eltiempo::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
