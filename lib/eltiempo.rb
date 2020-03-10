494a8a8e84f83d849231dc262e11b4ebe6038e14
Ruben <rubensere@gmail.com>
2020-03-10 17:52:42 +0100

    first commit

--- lib/eltiempo.rb
+++ lib/eltiempo.rb
@@ -1,6 +1,16 @@
-require "eltiempo/version"
+require "eltiempo"
+require 'thor'
+
 
 module Eltiempo
-  class Error < StandardError; end
-  # Your code goes here...
+  class CLI < Thor
+    desc "hello [name]", "say my name"
+    def hello(name)
+      if name == "Heisenberg"
+        puts "you are goddman right"
+      else
+        puts "say my name"
+      end
+    end
+  end
 end
