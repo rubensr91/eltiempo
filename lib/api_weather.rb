dcd7825ddbf745cbe90807854707ad480aa77aa2
Ruben <rubensere@gmail.com>
2020-03-11 23:44:56 +0100

    refactoring and cleaning

--- lib/api_weather.rb
+++ lib/api_weather.rb
@@ -1,85 +1,70 @@
 require 'net/http'
 require 'nokogiri'
-require 'date'
 
 class ApiWeather 
     @affiliate_id = '&affiliate_id=zdo2c683olan'
     @lang = "api_lang=en"
-    @temp_max
-    @temp_min
-    @temp_today
-    @today = Date.today
-    @province_url
 
-    def self.get_max_temp(province)
+    def self.get_max_temp(province_url)
 
-        get_province_url(province)
+        temp_max = []
 
-        xml_body = Net::HTTP.get_response(URI.parse(@province_url)).body
+        xml_body = Net::HTTP.get_response(URI.parse(province_url)).body
 
         doc = Nokogiri::XML(xml_body)
 
         doc.xpath('//var').each do |var|
             if (var.at_xpath('name').content.to_s.eql? 'Temperatura Máxima') || (var.at_xpath('name').content.to_s.eql? 'Maximum Temperature')
-                values = []
                 var.search('forecast').each do |forecast|
-                    values << forecast["value"].to_i
+                    temp_max << forecast["value"].to_i
                 end
-
-                @temp_max = values.max
             end
         end
 
-        @temp_max
+        temp_max
     end
 
-    def self.get_min_temp(province)
+    def self.get_min_temp(province_url)
+
+        temp_min = []
         
-        get_province_url(province)
-
-        xml_body = Net::HTTP.get_response(URI.parse(@province_url)).body
+        xml_body = Net::HTTP.get_response(URI.parse(province_url)).body
 
         doc = Nokogiri::XML(xml_body)
 
         doc.xpath('//var').each do |var|
-            
             if (var.at_xpath('name').content.to_s.eql? 'Temperatura Mínima') || (var.at_xpath('name').content.to_s.eql? 'Minimum Temperature')
-                values = []
                 var.search('forecast').each do |forecast|
-                    values << forecast["value"].to_i
+                    temp_min << forecast["value"].to_i
                 end
-
-                @temp_min = values.min
             end
         end
 
-        @temp_min
+        temp_min
     end
 
-    def self.get_today_temp(province)
+    def self.get_today_temp(province_url)
 
-        get_province_url(province)
+        today = []
 
-        xml_body = Net::HTTP.get_response(URI.parse(@province_url)).body
+        xml_body = Net::HTTP.get_response(URI.parse(province_url)).body
 
         doc = Nokogiri::XML(xml_body)
 
         doc.xpath('//var').each do |var|
-
             if (var.at_xpath('name').content.to_s.eql? 'Definición de Atmosfera') || (var.at_xpath('name').content.to_s.eql? 'Atmospheric condition')
-                values = []
                 var.search('forecast').each do |forecast|
-                    values << forecast["value"]
+                    today << forecast["value"]
                 end
-                
-                @temp_today = values[0]
             end
         end
 
-        @temp_today
+        today
     end
 
     def self.get_province_url(province)
+
+        province_url = nil
 
         url = "http://api.tiempo.com/index.php?#{@lang}&division=102#{@affiliate_id}"
 
@@ -89,22 +74,10 @@ class ApiWeather
         doc.xpath('//data').each do |data|
 
             if data.at_xpath('name').content.to_s.eql? province
-                @province_url = data.at_xpath('url').content.concat(@affiliate_id)
+                province_url = data.at_xpath('url').content.concat(@affiliate_id)
             end
         end
 
-        if @province_url == nil
-            puts "Province not found."
-        end
-    end
-
-    class Date
-        def dayname
-            DAYNAMES[self.wday]
-        end
-
-        def abbr_dayname
-            ABBR_DAYNAMES[self.wday]
-        end
+        province_url
     end
 end
--- lib/eltiempo.rb
+++ lib/eltiempo.rb
@@ -7,20 +7,19 @@ class EltiempoCLI
     command = argv[0]
     province = argv[1]
 
+    province_url = ApiWeather.get_province_url(province)
+    temp_min = ApiWeather.get_min_temp(province_url)
+    temp_max = ApiWeather.get_max_temp(province_url)
+    today = ApiWeather.get_today_temp(province_url)
+
     if (argv.empty?) || (command.eql? "help")
       puts "Try eltiempo -av_max 'Gavá'"
     elsif (command == "-av_max") 
-      temp = ApiWeather.get_max_temp(province)
-
-      puts "The maximum temperature of the week in #{province} is #{temp}°C"
+      puts "The maximum temperature of the week in #{province} is #{temp_max.max}°C"
     elsif (command == "-av_min")
-      temp = ApiWeather.get_min_temp(province)
-
-      puts "The minimum temperature of the week in #{province} is #{temp}°C"
+      puts "The minimum temperature of the week in #{province} is #{temp_min.min}°C"
     elsif (command == "-today")
-      temp = ApiWeather.get_today_temp(province)
-
-      puts "The Atmospheric condition in #{province} is #{temp}"
+      puts "Today #{province} will be #{today[0]} with \nminimum temperature of #{temp_min[0]}°C and maximum of #{temp_max[0]}°C"
     else
       puts "Command not accepted. \nRun 'eltiempo help' for more details."
     end
